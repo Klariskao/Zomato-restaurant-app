87bd66adc3446638a50e682eadc85993bef901ee
Klariskao <klara.ond@seznam.cz>
2022-02-10 12:42:02 +0000

    Request location permission

--- app/src/main/java/com/example/zomatoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/zomatoapp/ui/home/HomeFragment.kt
@@ -1,9 +1,16 @@
 package com.example.zomatoapp.ui.home
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,11 +21,14 @@ import com.example.zomatoapp.data.OkHttpRequest
 import com.example.zomatoapp.data.RestaurantViewModel
 import com.example.zomatoapp.databinding.FragmentHomeBinding
 
+// Fragment for home screen with RecyclerView of restaurants
 class HomeFragment: Fragment() {
 
+    // Set up binding variables
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    // View model variables
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var restaurantViewModel: RestaurantViewModel
 
@@ -30,8 +40,10 @@ class HomeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         _binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        requestLocationPermission(requireContext())
+
 
         // Make an http request to fetch data
         OkHttpRequest(this).makeRequest()
@@ -53,5 +65,43 @@ class HomeFragment: Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun requestLocationPermission(context: Context) {
+        val locationPermissionRequest = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                    // Precise location access granted
+                    Toast.makeText(context, "Location access granted", Toast.LENGTH_SHORT).show()
+                    Log.d("TAG", "Got location")
+                }
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                    // Only approximate location access granted
+                    Toast.makeText(context, "Location access granted", Toast.LENGTH_SHORT).show()
+                    Log.d("TAG", "Got location")
+                }
+                else -> {
+                    // No location access granted
+                    Toast.makeText(context, "Location access denied", Toast.LENGTH_SHORT).show()
+                    Log.d("TAG", "Got not location")
+                }
+            }
+        }
+
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                // You can use the API that requires the permission
+                Log.d("TAG", "Got location")
+            }
+            else -> {
+                locationPermissionRequest.launch(arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION))
+            }
+        }
     }
 }
